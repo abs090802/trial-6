228bd12afffed348e102c5ad726abe49e294caa9
bhavesh0908 <bhavesh.karapu@btech.christuniversity.in>
2023-03-21 23:56:31 +0530

    setup redux

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
-
+import {Provider} from "react-redux";
+import {store} from "./store";
 // 1) Set up redux
 
 
 
 export default function App() {
   return (
+    <Provider store={store}>
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
     </View>
+    </Provider>
   );
 }
 
