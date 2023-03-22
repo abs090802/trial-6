8dbe0d341ed9616f55cc37ab655d954d95000204
bhavesh0908 <bhavesh.karapu@btech.christuniversity.in>
2023-03-23 01:24:17 +0530

    2:13

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -3,6 +3,8 @@ import { FlatList, Text, TouchableOpacity, View, Image} from "react-native";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "@rneui/base";
 import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data= [
     {
@@ -22,6 +24,7 @@ const data= [
 
 const NavOptions = () => {
 
+    const origin = useSelector(selectOrigin);
     const naviagtion = useNavigation();
     return(
         <FlatList
@@ -32,8 +35,9 @@ const NavOptions = () => {
             <TouchableOpacity
             onPress={() => naviagtion.navigate(item.screen)}
             style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-100 m-2 w-40`}
+            disabled={!origin}
             >
-                <View>
+                <View style={tw`${!origin && "opacity-20"}`}>
                     <Image
                     style={{width:120, height:120, resizeMode: 'contain'}}
                     source={{uri:item.image}}
